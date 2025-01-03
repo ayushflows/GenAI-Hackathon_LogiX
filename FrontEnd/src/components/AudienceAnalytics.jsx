470182690b9bd5b57998c5aa92b96a06084ef160
Ayush Tripathi <mailtoayushtripathi@gmail.com>
2025-01-03 13:20:09 +0530

    last dashboard layout

---
+++ FrontEnd/src/components/AudienceAnalytics.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AudienceAnalytics() {
+  return (
+    <div id='Audience' className='px-16 mt-6'>
+    <h2 className='text-xl font-semibold'>Audience</h2>
+    </div>
+  )
+}
+
+export default AudienceAnalytics
