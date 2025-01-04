903c45e730f677ed44b9e2b26a7d236a1f07addc
Ayush Tripathi <mailtoayushtripathi@gmail.com>
2025-01-04 18:44:02 +0530

    updated responsiveness in dashboard

--- FrontEnd/src/pages/DashboardPage.jsx
+++ FrontEnd/src/pages/DashboardPage.jsx
@@ -5,8 +5,10 @@ import DashboardNavbar from '../components/DashboardNavbar';
 import AudienceAnalytics from '../components/AudienceAnalytics';
 import DashboardActivePlatform from '../components/DashboardActivePlatform';
 import DashboardInsights from '../components/DashboardInsights';
+import { useNavigate } from 'react-router-dom';
 
 function DashboardPage() {
+  const navigate = useNavigate();
   const [activeButton, setActiveButton] = useState('dashboard');
 
   const handleScroll = () => {
@@ -45,16 +47,20 @@ function DashboardPage() {
   return (
     <div className='w-[100vw] h-screen overflow-hidden dashboard-bg flex justify-center items-center inter-regular'>
       <div className='w-full h-full lg:w-[calc(100vw-15px)] lg:h-[calc(100vh-15px)] bg-[#2E3139] rounded-none lg:rounded-[40px] lg:flex overflow-hidden inter-regular'>
-        <div className='lg:w-[230px] lg:h-full w-full h-[100px] bg-[#22252D] flex flex-col'>
-          <div className='w-full h-[10%] text-black pl-6'>
-            <div className='flex mt-6 justify-start items-center'>
-              <img src={logoImg} alt='LogiX Logo' className='inline-block h-10 mr-2 filter invert contrast-200' />
-              <span className='dashboard-sketch-text'>LogiX</span>
+        <div className='lg:w-[230px] lg:h-full w-full h-[60px] bg-[#22252D] flex lg:flex-col'>
+          <div className='w-full h-full lg:h-[10%] text-black px-4 sm:px-8 lg:pl-6 flex justify-between items-center'>
+            <div className='flex lg:mt-6 justify-start items-center'>
+              <img src={logoImg} alt='LogiX Logo' className='inline-block h-[30px] lg:h-10 mr-2 filter invert contrast-200' />
+              <span className='dashboard-sketch-text text-[18px] lg:text-[1.7rem]'>LogiX</span>
+            </div>
+            <div className='flex lg:hidden gap-4 inter-regular'>
+              <button className='text-gray-300 hover:text-white sm:text-lg' onClick={()=>navigate("/")}>Home </button>
+              <button className='text-gray-300 hover:text-white sm:text-lg' onClick={()=>navigate("/userinput")}>Back</button>
             </div>
           </div>
           <DashboardNavbar setActiveButton={setActiveButton} activeButton={activeButton} onButtonClick={handleButtonClick}/>
         </div>
-        <div id='analytics-overview' className='lg:w-[calc(100%-230px)] lg:h-full w-full h-[calc(100vh-100px)] bg-[#2E3139] overflow-y-auto custom-scrollbar'>
+        <div id='analytics-overview' className='lg:w-[calc(100%-230px)] lg:h-full w-full h-[calc(100vh-60px)] bg-[#2E3139] overflow-y-auto custom-scrollbar'>
           <h1 className='text-2xl font-normal figtree-regular w-full py-3 text-center text-[#e6e6e6] mb-2'> Analytics Overview </h1>
             <DashboardActivePlatform />
           <section id="dashboard">
