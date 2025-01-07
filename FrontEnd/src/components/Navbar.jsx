29eca950e7b588aecd57b2b7b54130be64e0f22c
Ayush Tripathi <mailtoayushtripathi@gmail.com>
2025-01-07 16:32:11 +0530

    updated responsiveness on mobile

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
         <span className='sketch-text text-[18px] md:text-[1.7rem]'>LogiX</span>
       </div>
       <div className='flex space-x-5 md:space-x-8 justify-center items-center'>
-        <a href='/' className='text-gray-300 hover:text-[#F97315] text-[16px] md:text-lg '>Home</a>
+        <a href='/' className='text-gray-300 hover:text-[#F97315] text-[16px] md:text-lg hidden md:block'>Home</a>
         <a href='/contact' className='text-gray-300 hover:text-[#F97315] text-lg hidden md:block'>Contact</a>
         <a href='/userinput' className='text-gray-300 hover:text-[#F97315] text-[16px] md:text-lg'>Get Started</a>
         <button onClick={() => { window.open("https://github.com/ayushflows/GenAI-Hackathon_LogiX", "_blank");}} 
