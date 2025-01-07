76ef4b61d914476ba1e906a1ccf629449fe924fa
Nitesh saini <itsniteshsaini6376@gmail.com>
2025-01-07 23:15:33 +0530

    chat fixed

--- BackEnd/controllers/chatController.js
+++ BackEnd/controllers/chatController.js
@@ -7,7 +7,8 @@ module.exports.chat = async (req, res) => {
         const { chat } = req.body;
         let stringChat = JSON.stringify(chat);
         chat_result = await chat_main(stringChat);
-        res.send(chat_result.message.text);
+        // res.send(chat_result.message.text);
+        res.json({ message: chat_result.message.text });
     } catch (error) {
         console.error('Error during chat operation:', error);
         res.status(500).json({ error: 'Error during chat operation' });
