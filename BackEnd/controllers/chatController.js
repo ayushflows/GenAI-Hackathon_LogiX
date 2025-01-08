eb2fdd85b29a908cb15622e517df0d5f3b17fd5b
Nitesh saini <itsniteshsaini6376@gmail.com>
2025-01-08 15:53:24 +0530

    remove replace

--- BackEnd/controllers/chatController.js
+++ BackEnd/controllers/chatController.js
@@ -7,8 +7,8 @@ module.exports.chat = async (req, res) => {
         const { chat } = req.body;
         let stringChat = JSON.stringify(chat);
         let chat_result = await chat_main(stringChat);
-        let sanitized_result = chat_result.replace(/[^a-zA-Z0-9\s]/g, '').replace(/\s+/g, ' ').trim();
-        res.json({ message: sanitized_result });
+        // let sanitized_result = chat_result.replace(/[^a-zA-Z0-9\s]/g, '').replace(/\s+/g, ' ').trim();
+        res.json({ message: chat_result });
     } catch (error) {
         console.error('Error during chat operation:', error);
         res.status(500).json({ error: 'Error during chat operation' });
